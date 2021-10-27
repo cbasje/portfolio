18dbfa7d12e9b270a1d408de428981e4aaa174a8
cbasje <45468731+cbasje@users.noreply.github.com>
2021-10-28 01:32:27 +0200

    Update project.ts

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,7 +1,9 @@
 export interface Project {
-    [key: string]: string | undefined
+    [key: string]: string | Date | undefined
 	id: string;
 	title: string;
 	image: string;
+	date: Date;
+	keywords: string;
 	url?: string;
 }
