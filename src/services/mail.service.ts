8632d58e9ffa638f83d0690a92ac07acb3586d89
cbasje <45468731+cbasje@users.noreply.github.com>
2021-11-10 16:45:34 +0100

    Update mail.service.ts

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -26,17 +26,9 @@ export default {
 	addToAirtable(form: ContactForm) {
 		const baseUrl = 'https://portfolio-api-ten.vercel.app/api';
 		const url = baseUrl + '/airtable';
-
-		const data = {
-			to: 'sebastiaan@benjami.in',
-			from: 'sebastiaan@benjami.in',
-			replyTo: form.email,
-			subject: `Message from ${form.firstName} ${form.lastName}`,
-			text: `A message has been posted from ${form.firstName} ${form.lastName} at ${form.email}:`,
-			html: `<p>A message has been posted from ${form.firstName} ${form.lastName} at ${form.email}:</p><blockquote>${form.message}</blockquote>`,
-		};
+		
 		axios
-			.post(url, data)
+			.post(url, form)
 			.then(() => {
 				console.log('Email sent');
 			})
